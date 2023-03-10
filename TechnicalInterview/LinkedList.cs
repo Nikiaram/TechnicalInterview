d31796551439e7df72d977f2880f64669083a27d
Aram Nikolayan <anikolayan@authoritysoftware.com>
2023-03-10 17:39:02 +0400

    Technical Interview

---
+++ TechnicalInterview/LinkedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalInterview
+{
+    public class LinkedList
+    {
+        public Node head { get; set; }
+        public void Print()
+        {
+            Node current = head;
+
+            if (current != null )
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }    
+    }
+}
---
+++ TechnicalInterview/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+namespace TechnicalInterview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static Node Reverse(Node node)
+        {
+            Node prev = node, next = null;
+           
+            while (prev != null)
+            {
+                Node temp = prev.next;
+                prev.next = next;
+                next = prev;
+                prev = temp;    
+            }
+
+            node = next;
+
+            return node;
+        }
+    }
+}
